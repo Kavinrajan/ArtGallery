c1d8c324d8fb69399df6c5dc032db1fec1310313
Kavinrajan <kavinece53@gmail.com>
2023-01-15 22:22:12 +0530

    API Key security practices.

--- app/src/main/java/com/kavin/artgallery/utils/AppConstants.kt
+++ app/src/main/java/com/kavin/artgallery/utils/AppConstants.kt
@@ -1,8 +1,10 @@
 package com.kavin.artgallery.utils
 
+import com.kavin.artgallery.BuildConfig
+
 object AppConstants {
     object API {
         val PHOTOS_PER_PAGE = 20
-        val API_KEY = "Client-ID wxl8gDYQvKHvTcfTzfFJ2Fy0GoSuKoJovMopdieYBvk"
+        val API_KEY = BuildConfig.API_ID
     }
 }
