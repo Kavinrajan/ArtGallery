0f6b36483c3af9a64283a2b9195dd679342c824d
kavinrajan <Github@123>
2023-10-14 22:35:04 +0530

    Adobe core and analytics SDK MobileCore setup

--- app/src/main/java/com/kavin/artgallery/ArtGalleryApp.kt
+++ app/src/main/java/com/kavin/artgallery/ArtGalleryApp.kt
@@ -2,14 +2,30 @@ package com.kavin.artgallery
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.adobe.marketing.mobile.Analytics
+import com.adobe.marketing.mobile.Identity
+import com.adobe.marketing.mobile.Lifecycle
+import com.adobe.marketing.mobile.MobileCore
 import com.kavin.artgallery.utils.isNight
 import dagger.hilt.android.HiltAndroidApp
+
 
 @HiltAndroidApp
 class ArtGalleryApp: Application() {
     override fun onCreate() {
         super.onCreate()
         setupDayNightMode()
+        MobileCore.setApplication(this);
+        try {
+            Analytics.registerExtension()
+           // MobileServices.registerExtension() //Register Mobile Services with Mobile Core
+            Lifecycle.registerExtension()
+            Identity.registerExtension()
+            MobileCore.start(null)
+        } catch (e: Exception) {
+            //Log the exception
+        }
+
     }
     fun setupDayNightMode() {
         val mode = if (isNight()) {
